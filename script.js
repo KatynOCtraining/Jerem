1910f0d0b59c979ccaadbafc48c611fdc1412c46
KatynOCtraining <t.theveupolaris@gmail.com>
2024-06-06 23:08:25 +0200

    hero-banner_shadow

--- script.js
+++ script.js
@@ -49,3 +49,21 @@ document.addEventListener('DOMContentLoaded', (event) => {
     setInterval(nextSlide, 5000); // Change slide every 5 seconds
 });
 
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Définir un délai de 2500ms (2,5 secondes)
+    setTimeout(function() {
+      // Sélectionner l'élément avec la classe "loader"
+      var loader = document.querySelector('.loader');
+      // Appliquer une transition pour une disparition en douceur
+      loader.style.transition = 'opacity 0.5s ease';
+      // Définir l'opacité à 0 pour commencer la transition de disparition
+      loader.style.opacity = '0';
+      // Attendre que la transition se termine avant de retirer l'élément du DOM
+      setTimeout(function() {
+        loader.style.display = 'none';
+      }, 500); // 500ms correspond à la durée de la transition CSS
+    }, 2500); // 2500ms correspond au délai avant de commencer la disparition
+  });
